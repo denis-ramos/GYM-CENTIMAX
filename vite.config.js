84d0a53b6ca2678b386958ae7f16015518c7e0c6
denis ramos <pacohelmer@gmail.com>
2024-06-01 15:20:19 -0400

    page8

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://denis-ramos.github.io/gym_centimax/",
+  base:"https://denis-ramos.github.io/gym_centimax/"
 })
