ddef5f3434bac343a07bd71f90edae5bbf459aef
denis ramos <pacohelmer@gmail.com>
2024-06-01 15:59:14 -0400

    pagefin

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import Celular from './componentes/com21.1/Celular.jsx'
 import Inicio from './componentes/inicio/Inicio.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter basename="/gym_centimax/"> {/* Reemplaza 'tu-nombre-de-repo' con el nombre de tu repositorio */}
-      <Inicio />
-    </BrowserRouter>
+    <Inicio />
   </React.StrictMode>,
 )
