962fdce693f786e973778667fe4056c10dc859dd
denis ramos <pacohelmer@gmail.com>
2024-06-01 23:53:39 -0400

    navigation

--- src/componentes/Routers/Navigation.jsx
+++ src/componentes/Routers/Navigation.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Inicio from '../inicio/Inicio.jsx'
 import Inicio_de_secion from '../inicio_de_secion/Inicio_de_secion'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { HashRouter, Route, Router, Routes } from 'react-router-dom'
 function Navigation() {
   return (
-    <BrowserRouter basename='gym_centimax'>
+    <HashRouter >
         <Routes >
             <Route path="/" element={<Inicio/>} />
             <Route path="/Inicio_de_secion" element={<Inicio_de_secion/>} />
             <Route path="/*" element= {<p>Error 404 Recurso no encontrado</p>} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
